0a870af881b1a356939d630c7e3313bac90bf6ff
Benoit Larroque <benoit@sqreen.io>
2016-10-07 11:16:16 +0200

    Add highlighting of keyworks searched

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007075048) do
+ActiveRecord::Schema.define(version: 20161007110849) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
